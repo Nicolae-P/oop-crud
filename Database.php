7be747fee93d0d2dbf74d4115fc56e8370864981
Nicolae Pogila <nikupogila@gmail.com>
2024-04-18 17:35:34 +0300

    initial commit

---
+++ Database.php
@@ -0,0 +1,28 @@
+<?php
+class Database
+{
+    private static $instance = null;
+    private $connection;
+
+    private function __construct()
+    {
+        $this->connection = new mysqli('localhost', 'root', 'admin', 'crud');
+        if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+}
